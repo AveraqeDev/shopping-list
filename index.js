902461f8265dfda904b86a2f4ef86737cc623c40
Matthew Wagaman <averaqedev@gmail.com>
2020-01-08 13:56:41 -0800

    Finished project

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
 'use strict';
 
 function addItem() {
-  $('.js-shopping-list-form').on('submit', '.submit', (event) => {
-      console.log('Submit button clicked');
+  $('#js-shopping-list-form').submit((event) => {
+    event.preventDefault();
+    const item = $(event.currentTarget).find('input');
+    $(event.currentTarget).siblings('ul').append(`<li>
+    <span class="shopping-item">${item.val()}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle">
+        <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button>
+    </div>
+  </li>`);
+    item.val('');
   });
 }
 
 function checkItem() {
-  $('.shopping-item-controls').on('click', '.shopping-item-toggle', event => {
-    console.log('check button clicked');
-    $(this).closest('span').toggleClass('shopping-item__checked');
+  $('.shopping-list').on('click', '.shopping-item-toggle', event => {
+    $(event.currentTarget).parent().siblings('.shopping-item').toggleClass('shopping-item__checked');
+    event.preventDefault();
+  });
+}
+
+function deleteItem() {
+  $('.shopping-list').on('click', '.shopping-item-delete', event => {
+    $(event.currentTarget).closest('li').remove();
+    event.preventDefault();
   });
 }
 
 function main() {
   addItem();
   checkItem();
+  deleteItem();
 }
 
 $(main);
